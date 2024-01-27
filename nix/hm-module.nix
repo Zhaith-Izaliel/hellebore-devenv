12effe695eeb656529c9d7076cccbd0494f7d8af
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-01-27 01:37:17 +0100

    fix: add volar back

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -1,45 +1,51 @@
-{ package }:
-{ config, pkgs, lib, stdenv, ... }:
-
-let
+{package}: {
+  config,
+  pkgs,
+  lib,
+  stdenv,
+  ...
+}: let
   inherit (lib) mkEnableOption mkIf types mkOption;
   cfg = config.programs.helix.zhaith-configuration;
-  
+
   extraLanguages = ''
     [language-server]
     vuels = { command = "vue-language-server", args = ["--stdio"], config = { typescript = { tsdk = "${volar}/lib/node_modules/typescript/lib/" } } }
   '';
-  
-  nodejs-servers = (import ./nodejs {
+
+  nodejs-servers = import ./nodejs {
     inherit pkgs stdenv;
     nodejs = pkgs.nodejs;
-  });
-  
+  };
+
   volar = nodejs-servers."@volar/vue-language-server".overrideAttrs (final: prev: {
     nativeBuildInputs = with pkgs; [
       typescript
     ];
-  
-    installPhase = prev.installPhase + ''
-    mkdir -p $out/node_modules
 
-    ln -s ${pkgs.typescript}/lib/node_modules/* $out/node_modules
-    '';
+    installPhase =
+      prev.installPhase
+      + ''
+        mkdir -p $out/node_modules
+
+        ln -s ${pkgs.typescript}/lib/node_modules/* $out/node_modules
+      '';
   });
-in
-{
+in {
   options.programs.helix.zhaith-configuration = {
     enable = mkEnableOption "Zhaith Izaliel's Helix configuration";
 
-    defaultEditor = mkEnableOption null // {
-      description = ''
-        Whether to configure {command}`hx` as the default
-        editor using the {env}`EDITOR` environment variable.
-      '';
-    };
+    defaultEditor =
+      mkEnableOption null
+      // {
+        description = ''
+          Whether to configure {command}`hx` as the default
+          editor using the {env}`EDITOR` environment variable.
+        '';
+      };
 
     package = mkOption {
-      default = package.override { inherit extraLanguages; };
+      default = package.override {inherit extraLanguages;};
       type = types.package;
       description = "Defines the package used to get Helix's configuration from.";
     };
@@ -67,6 +73,7 @@ in
         ltex-ls
         nodejs-servers.stylelint-lsp
         nodejs-servers."@tailwindcss/language-server"
+        volar
 
         # DAP
         lldb
@@ -74,11 +81,10 @@ in
 
         # Formatters
         alejandra
-      ];    
+      ];
     };
-    
+
     home.file.".config/helix/config.toml".source = "${cfg.package}/config.toml";
     home.file.".config/helix/languages.toml".source = "${cfg.package}/languages.toml";
   };
 }
-
