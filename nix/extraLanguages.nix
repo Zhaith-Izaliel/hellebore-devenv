7d075e9b58de7f293d24d74f75e53e5354a381f4
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-04-02 12:13:59 +0200

    style(extraLanguages): remove unnecessary comments
    g

--- nix/extraLanguages.nix
+++ nix/extraLanguages.nix
@@ -6,17 +6,4 @@
 in ''
   [language-server.vuels.config.typescript]
   tsdk = "${pkgs.typescript}/lib/node_modules/typescript/lib/"
-
-  # [language-server.scls]
-  # command = "${pkgs.simple-completion-language-server}/bin/simple-completion-language-server"
-
-  # [language-server.nil]
-  # command = "${getExe pkgs.nil}"
-
-  # [language-server.taplo]
-  # command = "${getExe pkgs.taplo}"
-
-  # [[language]]
-  # name = "toml"
-  # formatter = { command = "${getExe pkgs.taplo}", args = ["format", "-"] }
 ''
