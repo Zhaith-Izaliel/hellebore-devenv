76652f674a5eda319bc03232fe47001ddc975239
Zhaith Izaliel <zhaith.izaliel@ethereal-edelweiss.cloud>
2025-03-20 15:57:57 +0100

    fix(helix/gdshader): add missing hash

--- helix/nix/dependencies/gdshader-lsp.nix
+++ helix/nix/dependencies/gdshader-lsp.nix
@@ -7,7 +7,7 @@ rustPlatform.buildRustPackage {
   pname = "gdshader-lsp";
   version = "f3847df";
 
-  cargoHash = "";
+  cargoHash = "sha256-kEHqmuuh+84wJ6yjcqGvSSSVEixGbSd9qE6Fg1IE/08=";
 
   meta.mainProgram = "gdshader-lsp";
 }
